3c0fa261511df141f473d8317145e309c8e58782
Jun <wnn156@naver.com>
2018-12-20 05:38:10 +0900

    integrated all

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/transition/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/transition/R.java
@@ -90,35 +90,6 @@ public final class R {
         public static final int ghost_view = 0x7f080068;
         public static final int icon = 0x7f08006e;
         public static final int icon_group = 0x7f08006f;
-<<<<<<< HEAD
-        public static final int info = 0x7f080076;
-        public static final int italic = 0x7f080078;
-        public static final int line1 = 0x7f08007e;
-        public static final int line3 = 0x7f08007f;
-        public static final int normal = 0x7f080091;
-        public static final int notification_background = 0x7f080092;
-        public static final int notification_main_column = 0x7f080093;
-        public static final int notification_main_column_container = 0x7f080094;
-        public static final int parent_matrix = 0x7f08009a;
-        public static final int right_icon = 0x7f0800b6;
-        public static final int right_side = 0x7f0800b7;
-        public static final int save_image_matrix = 0x7f0800b8;
-        public static final int save_non_transition_alpha = 0x7f0800b9;
-        public static final int save_scale_type = 0x7f0800ba;
-        public static final int tag_transition_group = 0x7f0800ec;
-        public static final int tag_unhandled_key_event_manager = 0x7f0800ed;
-        public static final int tag_unhandled_key_listeners = 0x7f0800ee;
-        public static final int text = 0x7f0800ef;
-        public static final int text2 = 0x7f0800f0;
-        public static final int time = 0x7f0800fb;
-        public static final int title = 0x7f0800fc;
-        public static final int transition_current_scene = 0x7f080102;
-        public static final int transition_layout_save = 0x7f080103;
-        public static final int transition_position = 0x7f080104;
-        public static final int transition_scene_layoutid_cache = 0x7f080105;
-        public static final int transition_transform = 0x7f080106;
-=======
-<<<<<<< HEAD
         public static final int info = 0x7f080075;
         public static final int italic = 0x7f080077;
         public static final int line1 = 0x7f08007d;
@@ -145,35 +116,6 @@ public final class R {
         public static final int transition_position = 0x7f080106;
         public static final int transition_scene_layoutid_cache = 0x7f080107;
         public static final int transition_transform = 0x7f080108;
-=======
-        public static final int info = 0x7f080074;
-        public static final int italic = 0x7f080076;
-        public static final int line1 = 0x7f08007c;
-        public static final int line3 = 0x7f08007d;
-        public static final int normal = 0x7f08008f;
-        public static final int notification_background = 0x7f080090;
-        public static final int notification_main_column = 0x7f080091;
-        public static final int notification_main_column_container = 0x7f080092;
-        public static final int parent_matrix = 0x7f080098;
-        public static final int right_icon = 0x7f0800b4;
-        public static final int right_side = 0x7f0800b5;
-        public static final int save_image_matrix = 0x7f0800b6;
-        public static final int save_non_transition_alpha = 0x7f0800b7;
-        public static final int save_scale_type = 0x7f0800b8;
-        public static final int tag_transition_group = 0x7f0800ea;
-        public static final int tag_unhandled_key_event_manager = 0x7f0800eb;
-        public static final int tag_unhandled_key_listeners = 0x7f0800ec;
-        public static final int text = 0x7f0800ed;
-        public static final int text2 = 0x7f0800ee;
-        public static final int time = 0x7f0800f9;
-        public static final int title = 0x7f0800fa;
-        public static final int transition_current_scene = 0x7f080100;
-        public static final int transition_layout_save = 0x7f080101;
-        public static final int transition_position = 0x7f080102;
-        public static final int transition_scene_layoutid_cache = 0x7f080103;
-        public static final int transition_transform = 0x7f080104;
->>>>>>> b41a1e2c51560ff5ced3bdffe5ab9fe450041a6a
->>>>>>> 820cd29d5de466b801cf2e53320b33d5199c1030
     }
     public static final class integer {
         private integer() {}
--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/v4/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/v4/R.java
@@ -110,33 +110,6 @@ public final class R {
         public static final int forever = 0x7f080065;
         public static final int icon = 0x7f08006e;
         public static final int icon_group = 0x7f08006f;
-<<<<<<< HEAD
-        public static final int info = 0x7f080076;
-        public static final int italic = 0x7f080078;
-        public static final int left = 0x7f08007c;
-        public static final int line1 = 0x7f08007e;
-        public static final int line3 = 0x7f08007f;
-        public static final int media_actions = 0x7f080087;
-        public static final int none = 0x7f080090;
-        public static final int normal = 0x7f080091;
-        public static final int notification_background = 0x7f080092;
-        public static final int notification_main_column = 0x7f080093;
-        public static final int notification_main_column_container = 0x7f080094;
-        public static final int right = 0x7f0800b5;
-        public static final int right_icon = 0x7f0800b6;
-        public static final int right_side = 0x7f0800b7;
-        public static final int start = 0x7f0800e2;
-        public static final int status_bar_latest_event_content = 0x7f0800e3;
-        public static final int tag_transition_group = 0x7f0800ec;
-        public static final int tag_unhandled_key_event_manager = 0x7f0800ed;
-        public static final int tag_unhandled_key_listeners = 0x7f0800ee;
-        public static final int text = 0x7f0800ef;
-        public static final int text2 = 0x7f0800f0;
-        public static final int time = 0x7f0800fb;
-        public static final int title = 0x7f0800fc;
-        public static final int top = 0x7f0800ff;
-=======
-<<<<<<< HEAD
         public static final int info = 0x7f080075;
         public static final int italic = 0x7f080077;
         public static final int left = 0x7f08007b;
@@ -161,33 +134,6 @@ public final class R {
         public static final int time = 0x7f0800fd;
         public static final int title = 0x7f0800fe;
         public static final int top = 0x7f080101;
-=======
-        public static final int info = 0x7f080074;
-        public static final int italic = 0x7f080076;
-        public static final int left = 0x7f08007a;
-        public static final int line1 = 0x7f08007c;
-        public static final int line3 = 0x7f08007d;
-        public static final int media_actions = 0x7f080085;
-        public static final int none = 0x7f08008e;
-        public static final int normal = 0x7f08008f;
-        public static final int notification_background = 0x7f080090;
-        public static final int notification_main_column = 0x7f080091;
-        public static final int notification_main_column_container = 0x7f080092;
-        public static final int right = 0x7f0800b3;
-        public static final int right_icon = 0x7f0800b4;
-        public static final int right_side = 0x7f0800b5;
-        public static final int start = 0x7f0800e0;
-        public static final int status_bar_latest_event_content = 0x7f0800e1;
-        public static final int tag_transition_group = 0x7f0800ea;
-        public static final int tag_unhandled_key_event_manager = 0x7f0800eb;
-        public static final int tag_unhandled_key_listeners = 0x7f0800ec;
-        public static final int text = 0x7f0800ed;
-        public static final int text2 = 0x7f0800ee;
-        public static final int time = 0x7f0800f9;
-        public static final int title = 0x7f0800fa;
-        public static final int top = 0x7f0800fd;
->>>>>>> b41a1e2c51560ff5ced3bdffe5ab9fe450041a6a
->>>>>>> 820cd29d5de466b801cf2e53320b33d5199c1030
     }
     public static final class integer {
         private integer() {}
