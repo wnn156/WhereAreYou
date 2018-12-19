6cbb9fb5e02820e5b6d8ff2e5961cdf372eff543
dongwook-94 <gygacpu@naver.com>
2018-12-20 05:26:13 +0900

    dongwook

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -76,19 +76,6 @@ public final class R {
         public static final int bottom = 0x7f08002a;
         public static final int end = 0x7f080050;
         public static final int gone = 0x7f080069;
-<<<<<<< HEAD
-        public static final int invisible = 0x7f080076;
-        public static final int left = 0x7f08007b;
-        public static final int packed = 0x7f080095;
-        public static final int parent = 0x7f080097;
-        public static final int percent = 0x7f08009a;
-        public static final int right = 0x7f0800b4;
-        public static final int spread = 0x7f0800db;
-        public static final int spread_inside = 0x7f0800dc;
-        public static final int start = 0x7f0800e1;
-        public static final int top = 0x7f080101;
-        public static final int wrap = 0x7f080115;
-=======
         public static final int invisible = 0x7f080075;
         public static final int left = 0x7f08007a;
         public static final int packed = 0x7f080094;
@@ -98,9 +85,8 @@ public final class R {
         public static final int spread = 0x7f0800da;
         public static final int spread_inside = 0x7f0800db;
         public static final int start = 0x7f0800e0;
-        public static final int top = 0x7f0800fd;
-        public static final int wrap = 0x7f080111;
->>>>>>> b41a1e2c51560ff5ced3bdffe5ab9fe450041a6a
+        public static final int top = 0x7f080100;
+        public static final int wrap = 0x7f080114;
     }
     public static final class styleable {
         private styleable() {}
